1e115a5f1d6e496eb0e6e0b0d40ec343b0ea5fc7
Adam DeCosta <adam.decosta@mymail.champlain.edu>
2018-04-17 00:24:46 -0400

    Allow server and client to connect to each other and talk

--- client.py
+++ client.py
@@ -2,14 +2,32 @@
 
 import argparse
 import asyncio
+import struct
 
 class ChatClient(asyncio.Protocol):
     
+    def __init__(self, loop):
+        self.loop = loop
+
+    def connection_made(self, transport):
+       self.transport = transport
+       print("Made Connection!")
+       
     def data_received(self, data):
         print("Received: " + data)
 
+    def send_message(self, message):
+        length = struct.pack('! I', len(message))
+        print(length)
+        payload = b''.join([length, message])
+        self.transport.write(payload)
+
+    def connection_lost(self, exc):
+        print('Server closed connection')
+        self.loop.stop()
+
 @asyncio.coroutine
-def handle_user_input(loop):
+def handle_user_input(loop, client):
     """ 
     reads from stdin in separate thread
     if user inputs 'quit' stops the event loop
@@ -19,7 +37,9 @@ def handle_user_input(loop):
         message = yield from loop.run_in_executor(None, input, "> ")
         if message == "quit":
             loop.stop()
-            return 
+            return
+        while True: 
+            client.send_message(message.encode('ASCII'))
         print(message)
 
 
@@ -33,11 +53,15 @@ if __name__ == "__main__":
 
     # Loop information and running
     loop = asyncio.get_event_loop()
+    print("{}, {}".format(args.host, args.p))
 
-    client = ChatClient()
+    client = ChatClient(loop)
 
-    coro = loop.create_connection(lambda: client, args.host, args.p)
-    loop.run_until_complete(handle_user_input(loop))
+    coro = loop.create_connection(lambda: client, 'localhost', 9000)
+    loop.run_until_complete(coro)
+
+    loop.run_until_complete(handle_user_input(loop, client))
+    
 
     try:
         loop.run_forever()
